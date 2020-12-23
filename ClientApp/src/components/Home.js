3ba3e34eb137bee00684a2bd1762929989311930
Aivis <aivis.palacis@gmail.com>
2020-12-23 22:05:24 +1300

    Git init - My first commit

---
+++ ClientApp/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.scss';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+      <div>
+            <img src="https://www.office2u.co.nz/images/2234/1920/banner-01.jpg" alt="doc" class="img-fluid" alt="Responsive image"></img>
+      </div>
+    );
+  }
+}
